a36db6866bab575d29f2432f756a0965ee2cc549
Kuljot Biring <ksbiring1@gmail.com>
2024-02-07 10:31:20 -0800

    add code for provider.tf

---
+++ IAC/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        aws = {
+            version ">=4.9.0"
+            source = "hashicorp/aws"
+        }
+    }
+}
+provider "aws" {
+    profile = "your_cli_profile"
+    access_key = "your_access_key"
+    secret_key = "your_secret_key"
+    region = "us-east-1"
+}
